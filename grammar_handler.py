cf6d4c061c05a9e3df4eabfecd19a16d0c4e751b
balkon16 <pawel.k.lonca@gmail.com>
2019-04-21 16:04:01 +0200

    file with CFG grammar is transformed into dictionaries

--- grammar_handler.py
+++ grammar_handler.py
@@ -10,19 +10,20 @@ class CFG_Error(Exception):
 
 def read_grammar_from_file(file_with_grammar):
     """
-    Given a text file with rules in CFG form the function returns
-    a dictionary indexed by the right-hand side and the left-hand side as
-    corresponding values.
-
-    The dictionary is based on CFG transformed into a grammar in Chomsky Normal
-    Form.
+    Given a text file with rules in CFG form the function returns a list of two
+    dictionaries:
+        1. terminals dictionary that contains (terminal, category) pairs
+        2. rules dictionary by the right-hand side and the left-hand side as
+        corresponding values.
+    The dictionaries contain rules in Context Free Grammar.
 
     It is assumed that the file is stored in the project's directory at the top
     level. Each line must contain at most one rule. Each rule must be in the
     following form:
         NP -> Det N
-        N -> "I"
+        N -> 'I'
         NP -> Det N PP
+    Please note the single quotes around non-terminal symbols.
 
     It is allowed that the right-hand side contains an alternative. The example
     listed above can be summarised as follows:
@@ -39,19 +40,25 @@ def read_grammar_from_file(file_with_grammar):
 
     try:
         if os.stat(file_with_grammar).st_size > 0:
-           print("File has contents.")
+           pass
         else:
            print("File is empty.")
     except OSError:
         print("No file {} found!".format(file_with_grammar))
 
-    # create empty dict that the rules will be stored in
+    # create empty dict that the rules will be stored in; covers rules that
+    # don't contain terminal symbols
     rules = dict()
+
+    # create empty dict that stores the terminal symbols and their categories
+    terminals = dict()
 
     with open(file_with_grammar, 'r') as file:
         for line_number, line in enumerate(file):
+
             # lines end with the new-line character "\n" - remove it
             line = line.rstrip()
+
             if line == "":
                 # ignoring an empty line
                 continue
@@ -70,6 +77,14 @@ def read_grammar_from_file(file_with_grammar):
 
                 # delete the trailing whitespace
                 lhs = lhs.rstrip()
+                # delete the leading whitespace
+                rhs = rhs.lstrip()
+                print(rhs)
+
+                if "'" in lhs:
+                    # left-hand side contains a terminal symbol
+                    raise CFG_Error("The rule must not contain a terminal " \
+                    "symbol on the left-hand side.", line_number + 1)
 
                 lhs_list = lhs.split(" ")
                 if len(lhs_list) > 1:
@@ -91,4 +106,22 @@ def read_grammar_from_file(file_with_grammar):
                     # right-hand side is empty
                     raise CFG_Error("Right-hand side is empty.", line_number + 1)
 
-            print(lhs," ", rhs)
+                if lhs.rstrip() == "":
+                    # left-hand side is empty
+                    raise CFG_Error("Left-hand side is empty.", line_number + 1)
+
+                for rhs_elem in rhs.split("|"):
+                    # if there is more than one right-hand side element then it is
+                    # isolated with a pipe ("|")
+
+                    rhs_elem = rhs_elem.strip()
+
+                    if "'" in rhs_elem:
+                        # terminal symbol on the right-hand side
+                        rhs_elem = rhs_elem.replace("'", "")
+                        terminals[rhs_elem] = lhs
+                    else:
+                        # non-terminal symbol
+                        rules[rhs_elem] = lhs
+
+        return [terminals, rules]
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import pprint
+
 from grammar_handler import *
 
-read_grammar_from_file("./dane_testowe/test_grammar.txt")
+pp = pprint.PrettyPrinter(indent=4)
+
+terminals, rules = read_grammar_from_file("./dane_testowe/test_grammar.txt")
+
+print("Terminals: ")
+pp.pprint(terminals)
+print("Rules: ")
+pp.pprint(rules)
