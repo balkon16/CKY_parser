6c5c3bb48ae9dbcf515f77fd6907f35c1ae2711c
balkon16 <pawel.k.lonca@gmail.com>
2019-04-23 12:26:09 +0200

    introduced manually made list of CNF rules

---
+++ acceptor.py
@@ -0,0 +1,16 @@
+lista = ['S -> NP VP',
+ 'PP -> P NP',
+ 'NP -> Det N',
+ 'NP -> X1 PP',
+ 'X1 -> Det N'
+ "NP -> 'I'",
+ 'VP -> V NP',
+ 'VP -> VP PP',
+ "Det -> 'an'",
+ "Det -> 'my'",
+ "N -> 'elephant'",
+ "N -> 'pajamas'",
+ "V -> 'shot'",
+ "P -> 'in'"]
+
+print(lista)
